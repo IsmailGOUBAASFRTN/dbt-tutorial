76838fc9580f0b5dbf4d39969496ae88518cac65
Ismail GOUBAA <igoubaa@sofrecom.fr>
2025-04-16 14:39:03 +0100

    Add customers model, tests, docs V2

--- models/customersall.sql
+++ models/customersall.sql
@@ -1,4 +1,4 @@
-with customers as (
+with customersall as (
 
     select * from {{ ref('stg_customers') }}
 
@@ -28,14 +28,14 @@ customer_orders as (
 final as (
 
     select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
+        customersall.customer_id,
+        customersall.first_name,
+        customersall.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders
 
-    from customers
+    from customersall
 
     left join customer_orders using (customer_id)
 
